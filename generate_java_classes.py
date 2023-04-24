d4c422e7dd5b9cd218fb02f151c503c4fa4252d0
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-04-24 20:16:57 -0300

    refactoring function to generate files

--- generate_java_classes.py
+++ generate_java_classes.py
@@ -2,34 +2,29 @@ import pandas as pd
 import markdown
 from bs4 import BeautifulSoup
 
+def createJava(name, code_blocks):
+    with open(f'java/{name}.java', "w") as f:
+        for j, code in enumerate(code_blocks):
+            code_text = code.get_text()
+            if "class" in code_text:
+                f.write(code_text)
+            elif code_text.count("\n") > 1:
+                f.write(f"public class MyClass{j} {{\n{code_text}\n}}")
+            f.write("\n\n")
+
 
 def generate_files():
     df = pd.read_csv('responses.csv')
-
     for i, row in df.iterrows():
         html = markdown.markdown(row['answer_chatgpt'],
                                  extensions=["fenced_code"])
         soup = BeautifulSoup(html, "html.parser")
         code_blocks = soup.find_all("code")
-
-        with open(f'RespostaGPT{i}.java', "w") as f:
-            for j, code in enumerate(code_blocks):
-                code_text = code.get_text()
-                if "class" in code_text:
-                    f.write(code_text)
-                else:
-                    f.write(f"public class MyClass{j} {{\n{code_text}\n}}")
-                f.write("\n\n")
-
+        createJava(f'RespostaGPT{i}',code_blocks)
         html = row['answer_stackoverflow']
         soup = BeautifulSoup(html, "html.parser")
         code_blocks = soup.find_all("code")
+        createJava(f'RespostaStackOverflow{i}',code_blocks)
 
-        with open(f'RespostaStackOverflow{i}.java', "w") as f:
-            for j, code in enumerate(code_blocks):
-                code_text = code.get_text()
-                if "class" in code_text:
-                    f.write(code_text)
-                else:
-                    f.write(f"public class MyClass{j} {{\n{code_text}\n}}")
-                f.write("\n\n")
+
+        
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import generate_java_classes as generator
 import time
 import pandas as pd
 
-questions = stack_overflow.get_questions(1)
+questions = stack_overflow.get_questions(10)
 responses = []
 for question in questions:
     gpt_question = "Generate a Java code to answer the following question: " + question[
         'title']
+    print(gpt_question)
     response = gpt.ask_gpt(gpt_question)
     question['answer_chatgpt'] = response
     responses.append(question)
